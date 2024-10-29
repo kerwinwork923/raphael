e09fdd4c4539400334dcb8ba9fe0261b95b56d09
kerwin <kerwinwork923@gmail.com>
2024-10-29 11:00:11 +0800

    t2.82

--- pages/user.vue
+++ pages/user.vue
@@ -38,7 +38,7 @@
           <div class="bottomTitle">積分</div>
         </div>
 
-        <div class="item item2">
+        <div class="item item2" @click="convertAndSaveUserData">
           <div class="topTitle">檢測</div>
           <div class="bottomTitle">HRV</div>
           <img src="../assets/imgs/faceIcon.svg" alt="" />
@@ -73,6 +73,149 @@
     </div>
   </div>
 </template>
+
+<script>
+import { onMounted, ref, onBeforeUnmount } from "vue";
+import { useRouter } from "vue-router";
+import Navbar from "../components/Navbar";
+import RaphaelLoading from "../components/RaphaelLoading";
+import axios from "axios";
+//圖片
+import banner1 from "@/assets/imgs/banner-1.png";
+import banner2 from "@/assets/imgs/banner-2.png";
+
+export default {
+  components: { Navbar, RaphaelLoading },
+  setup() {
+    const router = useRouter();
+    const loading = ref(true);
+    const userInfo = ref(null);
+    const currentSlide = ref(0);
+    const slides = ref([banner1, banner2]);
+
+    const getUserData = async () => {
+      const localData = localStorage.getItem("userData");
+      const { MID, Token, MAID, Mobile, Name } = localData
+        ? JSON.parse(localData)
+        : {};
+
+      if (!MID || !Token || !MAID || !Mobile) {
+        router.push("/login");
+        return;
+      } else if (Name === "") {
+        router.push("/changeMember");
+        return;
+      }
+      try {
+        const response = await axios.post(
+          "https://23700999.com:8081/HMA/API_AA6.jsp",
+          {
+            MID,
+            Token,
+            MAID,
+            Mobile,
+          }
+        );
+
+        if (response.status === 200) {
+          const data = response.data;
+          if (data.Result === "OK" && data.Member) {
+            const existingData = localData ? JSON.parse(localData) : {};
+            const newUserInfo = {
+              ...existingData,
+              ...data.Member,
+            };
+            localStorage.setItem("userData", JSON.stringify(newUserInfo));
+            userInfo.value = newUserInfo;
+          } else {
+            alert("取得會員資料失敗");
+          }
+        } else {
+          alert("取得會員資料失敗");
+        }
+      } catch (err) {
+        alert("取得會員資料失敗");
+      } finally {
+        setTimeout(() => {
+          loading.value = false;
+        }, 300);
+      }
+    };
+
+    getUserData();
+
+    const changeSlide = () => {
+      currentSlide.value = (currentSlide.value + 1) % slides.value.length;
+    };
+
+    let slideInterval;
+
+    onMounted(() => {
+      // 開始幻燈片循環
+      slideInterval = setInterval(() => {
+        changeSlide();
+      }, 3000);
+    });
+
+    onBeforeUnmount(() => {
+      clearInterval(slideInterval);
+    });
+
+    const convertAndSaveUserData = async () => {
+      const localData = JSON.parse(localStorage.getItem("userData"));
+
+      let scanAge = localData.SexF;
+      if (localData.Sex == 2) {
+        scanAge = 0;
+      }
+
+      // 確認是否存在用戶數據
+      if (localData) {
+        const convertedData = {
+          age: calculateAge(localData.Birthday), // 計算年齡的輔助函數
+          bp_group: "normal",
+          bp_mode: "ternary",
+          facing_mode: "user",
+          height: parseInt(localData.Height) || 170,
+          sex: scanAge,
+          weight: parseInt(localData.Weight) || 60,
+        };
+
+        // 保存轉換後的數據到 session storage
+        sessionStorage.setItem("data", JSON.stringify(convertedData));
+
+        try {
+          // 請求相機權限
+          await navigator.mediaDevices.getUserMedia({ video: true });
+          alert("相機已啟用。請在下一步繼續測量。");
+
+          // 跳轉到目標頁面
+          window.location.href = "/vital/scan.html";
+        } catch (err) {
+          alert("無法訪問相機。請檢查您的權限設置。");
+        }
+      } else {
+        alert("本地存儲中沒有用戶數據。");
+      }
+    };
+
+    // Helper function to calculate age based on Birthday
+    const calculateAge = (birthday) => {
+      const [year, month, day] = birthday.split("/").map(Number);
+      const currentYear = new Date().getFullYear();
+      return currentYear - (1911 + year); // Adjust for ROC year format
+    };
+
+    return {
+      loading,
+      userInfo,
+      currentSlide,
+      slides,
+      convertAndSaveUserData,
+    };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 .raphaelUser {
@@ -209,7 +352,11 @@
       }
       .item2 {
         background-color: $raphael-purple-200;
-        filter: blur(2px);
+        opacity: 1;
+        cursor: pointer;
+        &:hover {
+          filter: brightness(0.95);
+        }
       }
       .item3 {
         background-color: $raphael-cyan-400;
@@ -235,100 +382,3 @@
   }
 }
 </style>
-
-<script>
-import { onMounted, ref, onBeforeUnmount } from "vue";
-import { useRouter } from "vue-router";
-import Navbar from "../components/Navbar";
-import RaphaelLoading from "../components/RaphaelLoading";
-import axios from "axios";
-//圖片
-import banner1 from "@/assets/imgs/banner-1.png";
-import banner2 from "@/assets/imgs/banner-2.png";
-
-export default {
-  components: { Navbar, RaphaelLoading },
-  setup() {
-    const router = useRouter();
-    const loading = ref(true);
-    const userInfo = ref(null);
-    const currentSlide = ref(0);
-    const slides = ref([banner1, banner2]);
-
-    const getUserData = async () => {
-      const localData = localStorage.getItem("userData");
-      const { MID, Token, MAID, Mobile, Name } = localData
-        ? JSON.parse(localData)
-        : {};
-
-      if (!MID || !Token || !MAID || !Mobile) {
-        router.push("/login");
-        return;
-      } else if (Name === "") {
-        router.push("/changeMember");
-        return;
-      }
-      try {
-        const response = await axios.post(
-          "https://23700999.com:8081/HMA/API_AA6.jsp",
-          {
-            MID,
-            Token,
-            MAID,
-            Mobile,
-          }
-        );
-
-        if (response.status === 200) {
-          const data = response.data;
-          if (data.Result === "OK" && data.Member) {
-            const existingData = localData ? JSON.parse(localData) : {};
-            const newUserInfo = {
-              ...existingData,
-              ...data.Member,
-            };
-            localStorage.setItem("userData", JSON.stringify(newUserInfo));
-            userInfo.value = newUserInfo;
-          } else {
-            alert("取得會員資料失敗");
-          }
-        } else {
-          alert("取得會員資料失敗");
-        }
-      } catch (err) {
-        alert("取得會員資料失敗");
-      } finally {
-        setTimeout(() => {
-          loading.value = false;
-        }, 300);
-      }
-    };
-
-    getUserData();
-
-    const changeSlide = () => {
-      currentSlide.value = (currentSlide.value + 1) % slides.value.length;
-    };
-
-    let slideInterval;
-
-    onMounted(() => {
-      // 開始幻燈片循環
-      slideInterval = setInterval(() => {
-        changeSlide();
-      }, 3000);
-    });
-
-    onBeforeUnmount(() => {
-      clearInterval(slideInterval);
-    });
-
-    return {
-      loading,
-      userInfo,
-      currentSlide,
-      slides,
-    };
-  },
-};
-</script>
