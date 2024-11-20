66df502425982a9f0358cb71a95ae9980f71d83f
Wing <wingchen@raphael.clinic>
2024-11-20 15:12:28 +0800

    SDNNI改為SDNN，取最後13筆資料

--- public/vital/js/finish.js
+++ public/vital/js/finish.js
@@ -219,7 +219,7 @@ const GoToFinish = () => {
 
   _finish_vital_result_printer.update({
     hr: result.hr,
-    hrv: result.hrv_indices.SDNNI,
+    hrv: result.hrv_indices.SDNN,
     sbp: result.sbp,
     dbp: result.dbp,
     rr: result.rr,
