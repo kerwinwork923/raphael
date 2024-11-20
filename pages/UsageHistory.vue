6f222e0a554dc5630961e774f1efbe21dcf284fe
kerwin <kerwinwork923@gmail.com>
2024-11-20 15:46:43 +0800

    fix 圖片路徑

---
+++ pages/UsageHistory.vue
@@ -0,0 +1,12 @@
+<template>
+  <TitleMenu Text="使用紀錄" link="/user" />
+  .
+</template>
+
+<script>
+// import { TitleMenu } from "~/component/TitleMenu.vue";
+
+export default {
+  setep() {},
+};
+</script>
