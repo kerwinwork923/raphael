bd365969d26304f03ea051018a6a229b412a8ee8
kerwin <kerwinwork923@gmail.com>
2024-12-17 14:21:24 +0800

    補上標籤

--- components/TagList.vue
+++ components/TagList.vue
@@ -7,6 +7,9 @@
     <router-link to="/userRecord" class="listItem" active-class="listActive"
       >生活紀錄</router-link
     >
+    <router-link to="/weekly2" class="listItem" active-class="listActive"
+      >寶貝紀錄</router-link
+    >
     <!-- <router-link to="#" class="listItem" >寶貝紀錄</router-link> -->
   </div>
 </template>
@@ -16,7 +19,7 @@
   display: grid;
   grid-auto-flow: column;
   gap: 4px;
-  
+
   a {
     text-decoration: none;
   }
@@ -29,7 +32,7 @@
     color: $raphael-gray-500;
     text-align: center;
     cursor: pointer;
-    transition: all .3s ease;
+    transition: all 0.3s ease;
     &:hover {
       background-color: $raphael-green-300;
     }
@@ -37,7 +40,7 @@
   .listActive {
     background-color: $raphael-green-400;
     color: #fff;
-    transition: all .3s ease;
+    transition: all 0.3s ease;
     &:hover {
       background-color: $raphael-green-400;
       color: #fff;
