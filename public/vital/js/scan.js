b3c2ce67969332eb070c93ee668d0c45939c5a9e
kerwin <kerwinwork923@gmail.com>
2025-05-20 10:26:51 +0800

    更新

--- public/vital/js/scan.js
+++ public/vital/js/scan.js
@@ -161,7 +161,7 @@ const OnResult = (result) => {
     parseInt(proc) + "%";
 
   console.log(proc, result.scanning_status);
-  console.log(result)
+  console.log(result);
   nHRV.push([
     result.hrv_indices.HF,
     result.hrv_indices.LF,
@@ -225,8 +225,8 @@ const OnResult = (result) => {
       rr: result.rr,
       spo2: result.spo2,
       si: result.si,
-      cardiovascular_age : result.cardiovascular_age,
-      cardiovascular_bmi : result.cardiovascular_bmi,
+      cardiovascular_age: result.cardiovascular_age,
+      cardiovascular_bmi: result.cardiovascular_bmi,
       hr_valid: result.signal_quality.hr_hrv > 0.7,
       bp_valid: result.signal_quality.bp > 0.6,
       rr_valid: result.signal_quality.resp > 0.7,
@@ -272,9 +272,8 @@ const OnResult = (result) => {
     sessionStorage.setItem("Age", JSON.stringify(nArray));
     sessionStorage.setItem("rHRV", JSON.stringify(nHRV));
 
-
-   result.cardiovascular_bmi =
-     result.cardiovascular_bmi ?? calcBMI(infodata.height, infodata.weight);
+    result.cardiovascular_bmi =
+      result.cardiovascular_bmi ?? calcBMI(infodata.height, infodata.weight);
 
     document.removeEventListener("visibilitychange", OnVisibilityChange);
     StoptMeasuring("finish.html");
@@ -323,9 +322,22 @@ const OnEvent = (x) => {
 };
 
 const StartMeasuring = () => {
-  let user = JSON.parse(sessionStorage.getItem("data"));
-  console.log(user);
-  FHVitalsSDK.startMeasuring(user).then((result) => {
+  let user = JSON.parse(sessionStorage.getItem("data") || "{}");
+
+  const payload = {
+    height: Number(user.height),
+    weight: Number(user.weight),
+    sex: Number(user.sex),
+    age: Number(user.age),
+    bp_mode: user.bp_mode,
+    bp_group: user.bp_group,
+    virtual_id: user.virtual_id,
+  };
+
+  // 這一行就是 alert 整個 payload 給你看
+  alert(`Start Measuring Payload:\n${JSON.stringify(payload, null, 2)}`);
+
+  FHVitalsSDK.startMeasuring(payload).then((result) => {
     if (result.error == ERROR_CODE.NONE) {
       show(VIEW_SCAN_PROGRESS);
       show(VIEW_SCAN_PROGRESS_VALUE);
@@ -338,6 +350,23 @@ const StartMeasuring = () => {
     }
   });
 };
+
+// const StartMeasuring = () => {
+//   let user = JSON.parse(sessionStorage.getItem("data"));
+//   console.log(user);
+//   FHVitalsSDK.startMeasuring(user).then((result) => {
+//     if (result.error == ERROR_CODE.NONE) {
+//       show(VIEW_SCAN_PROGRESS);
+//       show(VIEW_SCAN_PROGRESS_VALUE);
+//       hide(VIEW_SCAN_P_HINT);
+//       document.getElementById(VIEW_SCANNING_STATUS).innerHTML = "";
+//       document.getElementById(VIEW_SCAN_PROGRESS_VALUE).innerHTML = "0%";
+//     } else {
+//       alert(`start measuring failed, reason=${result.error}`);
+//       StoptMeasuring("/user");
+//     }
+//   });
+// };
 const StoptMeasuring = (html_page) => {
   const { UID, flag, form } = getUrlParams();
 
@@ -446,8 +475,6 @@ function getUrlParams() {
 }
 
 _finish_vital_result_printer.update({
- 
-  cardiovascular_age : result.cardiovascular_age,
-  cardiovascular_bmi : result.cardiovascular_bmi,
-
+  cardiovascular_age: result.cardiovascular_age,
+  cardiovascular_bmi: result.cardiovascular_bmi,
 });
