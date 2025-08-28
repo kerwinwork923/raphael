3b34b5bf9146b47287c85a8b8b6309fb35676150
kerwin <kerwinwork923@gmail.com>
2025-08-28 10:09:18 +0800

    會員樣式

--- pages/member.vue
+++ pages/member.vue
@@ -6,7 +6,9 @@
       </div>
       <div class="memberTopRight">
         <h3>{{ userDataObj.Member.Name }} 您好</h3>
-        <div class="memberTopPoint">目前積分 : {{ userDataObj.NowAvaPoints }}</div>
+        <div class="memberTopPoint">
+          目前積分 : {{ userDataObj.NowAvaPoints }}
+        </div>
       </div>
     </div>
     <div class="memberCenter">
@@ -25,6 +27,40 @@
         </div>
       </div>
     </div>
+
+    <div class="memberListGroup">
+      <div class="memberListItem">
+        <div class="memberListLeft">
+          <img src="../assets/imgs/member/setting.svg" alt="" />
+          <h3>基本資料</h3>
+        </div>
+        <img src="../assets/imgs/member/next_green.svg" alt="" />
+      </div>
+      <div class="memberListItem">
+        <div class="memberListLeft">
+          <img src="../assets/imgs/member/privacy.svg" alt="" />
+          <h3>隱私權設定</h3>
+        </div>
+        <img src="../assets/imgs/member/next_green.svg" alt="" />
+      </div>
+      <div class="memberListItem">
+        <div class="memberListLeft">
+          <img src="../assets/imgs/member/warning.svg" alt="" />
+          <h3>免責聲明</h3>
+        </div>
+        <img src="../assets/imgs/member/next_green.svg" alt="" />
+      </div>
+      <div class="memberListItem">
+        <div class="memberListLeft">
+          <img src="../assets/imgs/member/about.svg" alt="" />
+          <h3>關於</h3>
+        </div>
+        <img src="../assets/imgs/member/next_green.svg" alt="" />
+      </div>
+    </div>
+
+    <button class="logoutBtn" @click="logout">登出</button>
+    <div class="deleteBtn" @click="deleteBtn">刪除帳號</div>
 
     <BottomNav />
   </div>
@@ -46,6 +82,15 @@ const goToPoint = () => {
 const goToPackage = () => {
   router.push("/package");
 };
+
+const logout = () => {
+  localStorage.removeItem("userData");
+  router.push("/");
+};
+
+const deleteBtn = () => {
+  console.log("刪除帳號");
+};
 </script>
 
 <style lang="scss">
@@ -57,7 +102,7 @@ const goToPackage = () => {
 
   width: 100%;
   min-height: 100vh;
-  padding: 1.5rem 1rem;
+  padding: 1.5rem 1rem 7rem;
   .memberTop {
     display: flex;
     align-items: center;
@@ -125,13 +170,14 @@ const goToPackage = () => {
         font-style: normal;
         font-weight: 700;
         line-height: 100%; /* 24px */
+        margin-top: 0.25rem;
         letter-spacing: 0.12px;
       }
       .memberCenterButton {
         display: flex;
         align-items: center;
         justify-content: center;
-        gap: 0.25rem;
+        gap: 0.15rem;
         border-radius: var(--Radius-r-50, 50px);
         background: linear-gradient(
           90deg,
@@ -142,10 +188,88 @@ const goToPackage = () => {
           2px 2px 6px 0
             var(--secondary-300-opacity-70, rgba(177, 192, 216, 0.7));
         color: #fff;
-        padding: 0.25rem 1rem;
-        margin-top: .5rem;
+        padding: 0.5rem 1rem;
+        margin-top: 0.75rem;
+
+        color: var(--Neutral-white, #fff);
+
+        font-size: var(--Text-font-size-18, 18px);
+        font-style: normal;
+        font-weight: 400;
+
+        letter-spacing: 2.7px;
+        width: 100%;
+        img {
+          transform: translateY(2px);
+        }
       }
     }
+  }
+  .memberListGroup {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-top: 1.5rem;
+    .memberListItem {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 1rem;
+      border-radius: var(--Radius-r-50, 50px);
+      background: var(--Secondary-100, #f5f7fa);
+      box-shadow: 2px 4px 12px 0
+        var(--secondary-300-opacity-40, rgba(177, 192, 216, 0.4));
+      .memberListLeft {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+        h3 {
+          color: var(--Neutral-black, #1e1e1e);
+          text-overflow: ellipsis;
+
+          font-size: var(--Text-font-size-18, 18px);
+          font-style: normal;
+          font-weight: 400;
+
+          letter-spacing: 2.7px;
+        }
+        img {
+          width: 24px;
+          height: 24px;
+        }
+      }
+    }
+  }
+  .logoutBtn {
+    width: 100%;
+    border-radius: var(--Radius-r-50, 50px);
+    background: var(--Secondary-100, #f5f7fa);
+    box-shadow: 2px 4px 12px 0
+      var(--secondary-300-opacity-70, rgba(177, 192, 216, 0.7));
+    border: none;
+    padding: 4px;
+    margin-top: 3rem;
+    border: 1px solid var(--Warning-default, #ec4f4f);
+    color: var(--Warning-default, #ec4f4f);
+    font-size: var(--Text-font-size-18, 18px);
+    font-style: normal;
+    font-weight: 400;
+    letter-spacing: 2.7px;
+  }
+  .deleteBtn {
+    width: 100%;
+    border-radius: var(--Radius-r-50, 50px);
+    background: var(--Warning-default, #ec4f4f);
+    box-shadow: 2px 4px 12px 0
+      var(--secondary-300-opacity-70, rgba(177, 192, 216, 0.7));
+    color: var(--Neutral-white, #fff);
+    font-size: var(--Text-font-size-18, 18px);
+    font-style: normal;
+    font-weight: 400;
+    letter-spacing: 2.7px;
+    text-align: center;
+    padding: 8px;
+    margin-top:3rem;
   }
 }
 </style>
