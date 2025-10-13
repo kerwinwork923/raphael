4c81ec82c7509f4920e3107a7b5a41e18309f26e
Steven <stevenyeh@raphael.clinic>
2025-10-13 16:08:52 +0800

    fix: style

--- pages/changeMember.vue
+++ pages/changeMember.vue
@@ -195,7 +195,7 @@ export default {
     flex-direction: column;
     place-items: center;
     width: 100%;
-    padding: 0 1rem 56px 1rem;
+    padding: 0.5rem 1rem 56px 1rem;
     height: 100vh;
     overflow: hidden;
   @include gradientBg();
